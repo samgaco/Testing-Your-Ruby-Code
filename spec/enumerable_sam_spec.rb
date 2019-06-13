ee5d1ce81e1c5a34e6130f069fc63f642cd14d2a
Samuel <samuelgarciacompanys@gmail.com>
2019-06-13 15:42:02 +0200

    tested my_count method

--- spec/enumerable_sam_spec.rb
+++ spec/enumerable_sam_spec.rb
@@ -18,6 +18,30 @@ RSpec.describe Enumerable do
       expect([1,2,3,4,5].my_select{|x| x.even?}).to eq([2, 4])
     end
   end
+
+  #testing my_count 
+
+  describe "#my_count" do
+    it 'counts the total number of elements, returns 5' do
+      # dc = DummyClass.new
+      expect([3,3,3,3,5].my_count()).to eq(5)
+    end
+  end
+
+  describe "#my_count" do
+    it 'counts the total number of "1" elements in the array, returns 2' do
+      # dc = DummyClass.new
+      expect([3,1,3,1,5].my_count(1)).to eq(2)
+    end
+  end
+
+  describe "#my_count" do
+    it 'counts the total number of even numbers, will return 1 ' do
+      # dc = DummyClass.new
+      expect([3,3,2,3,5].my_count{ |x| x%2 == 0 }).to eq(1)
+    end
+  end
+
 end
  
 
